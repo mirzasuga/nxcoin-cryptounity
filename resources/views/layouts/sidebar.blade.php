156717a5cdaec164c6cc0883c9ff17579a85eb6d
mirzasuga <sugamirza2@gmail.com>
2018-07-10 21:26:41 +0700

    c

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -20,6 +20,11 @@
                 </a>
             </li>
             <li class="nav-item">
+                <a class="nav-link" href="home">
+                    Home
+                </a>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link" href="package">
                     Packages
                 </a>
@@ -27,12 +32,6 @@
             <li class="nav-item">
                 <a class="nav-link" href="stacking">
                     Stacking
-                </a>
-            </li>
-            
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('dashboard') }}">
-                    Dashboard
                 </a>
             </li>
             <li class="nav-item">
