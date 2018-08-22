7c5158c72fad0c6485800db0fdc21295e09d6975
mirzasuga <sugamirza2@gmail.com>
2018-08-22 21:40:47 +0700

    added feature download as csv, etc in monitoring user balance

--- resources/views/monitoring/index.blade.php
+++ resources/views/monitoring/index.blade.php
@@ -61,11 +61,36 @@
 
 @section('inc_js')
 
+<!-- <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.flash.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js"></script>
 <script>
 $(document).ready(function() {
-    $('#userbalance').DataTable();
+    $('#userbalance').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend : 'csv',
+                text : 'Download CSV'
+            },
+            {
+                extend : 'excel',
+                text : 'Download Excel'
+            },
+            {
+                extend : 'pdf',
+                text : 'Download PDF'
+            },
+            'print'
+        ]
+    });
 } );
 </script>
 
